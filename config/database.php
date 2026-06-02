e3fea2bc8e258a577f3e14faef71745957aa6f5d
gianAchei <gian@acheipneus.com.br>
2026-06-01 21:54:55 -0300

    started prject

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$database = "db-atendelab";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$database;chartset-utf8", $user, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Conexão bem sucedida!";
+} catch (PDOException $e) {
+    // echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
+    die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
+}
---
+++ database/db-atendelab.sql
@@ -0,0 +1,18 @@
+REATE TABLE usuarios (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nome VARCHAR(100) NOT NULL,
+ email VARCHAR(100) NOT NULL UNIQUE,
+ senha VARCHAR(255) NOT NULL,
+ perfil ENUM('admin', 'atendente') DEFAULT 'atendente',
+ status ENUM('ativo', 'inativo') DEFAULT 'ativo',
+ criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO usuarios (nome, email, senha, perfil, status)
+VALUES (
+ 'Administrador',
+ 'admin@atendelab.com',
+ '$2y$10$J9P2kU2BAMZ3TZcuxTsW4e1D/lka8EocYHzvyoOZmCNcWDQz3RuVC',
+ 'admin',
+ 'ativo'
+);
